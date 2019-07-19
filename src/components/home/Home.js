2c927172f0b12f5f190d063b082d25b3064aa07c
G <jay122493@gmail.com>
2019-07-19 15:07:35 -0400

    top part done

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -8,7 +8,20 @@ export default class Home extends Component {
         return (
             <div className="home-page">
                     <Navbar/>
+                    <div className="blue-tint"></div>
+                    <div className="slogan-container">
+                        <h2>ENVIROMENTAL CLEANING SERVICE</h2>
+                        <h1>SERVING THE NEW JERSEY AREA FOR OVER 10 YEARS</h1>
+                        <h3>The best service at the best price! An enviromentally concious cleaning service that fits your needs. One call cleans it all!</h3>
+                        <a href=""><span>+</span><span> READ MORE</span></a>
+                    </div>
+                    <div className="call-container">
+                        <h3>Call Us - 24/7</h3>
+                        <h1>+1 888-123-1234</h1>
+                        <i class="fas fa-phone"></i>
+                    </div>
                     <Slideshow/>
+                    <i class="fas fa-phone"></i>
             </div>
         );
     }
--- src/components/slideshow/Slideshow.js
+++ src/components/slideshow/Slideshow.js
@@ -4,53 +4,19 @@ import girl from './images/girl-with-mop.jpg';
 import guy from './images/guy-wipe-windows.jpg';
 import swifer from './images/swifer.jpg';
 import wash from './images/wash-toilet.jpg';
+import floors from './images/floors.jpg';
 
 export default class Slideshow extends Component {
-
-    constructor(props) {
-        super(props);
-        this.getImageId = this.getImageId.bind(this);
-        this.switchImage = this.switchImage.bind(this);
-        this.init = this.init.bind(this);
-        this.state = {
-            currentImage: 0,
-            image: 0
-
-        };
-    }
-
-    getImageId() {
-        if(this.currentImage < 3) {
-            this.setState({
-                currentImage: this.state.currentImage +1
-            })
-        } else {
-            this.setState({
-                currentImage: 0
-            })
-        }
-        return this.currentImage;
-    }
-
-    switchImage() {
-            this.setState({
-                image: this.getImageId()
-            });
-    }
-
-    init() {
-        setInterval(this.switchImage, 1000)
-    }
-
     
     render() {
-        const imagePath = [guy, girl, wash, swifer];
-
-        this.init();
 
         return (
             <div className="slideshow-container">
-                <img src={imagePath[this.state.image]} alt="cleaning images"/>      
+                <img src={guy} alt="window wiper"/> 
+                <img src={girl} alt="girl with mop"/>
+                <img src={wash} alt="cleaning toilet"/>
+                <img src={swifer} alt="swifer"/> 
+                <img src={floors} alt="floor cleaner"/>
             </div>
         );
     }
